a9cf37e2a8c80f0a4176de15a2ff5e4ea7774f2e
william-alvine <alvinraila@outlook.com>
2026-09-18 22:09:39 +0300

    Add files via upload

---
+++ Introduction to programming.cpp
@@ -0,0 +1,22 @@
+// single line comment
+// program to display the units on offer
+/*
+Author: William
+Reg No: BCS-005-0219/2026
+Description: Units on offer
+Date: 09/09/2026
+Version 1
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Units Offered This Semester:\n");
+    printf("1. Introduction to Programming\n");
+    printf("2. Data Structures\n");
+    printf("3. Computer Networks\n");
+    printf("4. Database Systems\n");
+    printf("5. Software Engineering\n");
+
+    return 0;
+}
